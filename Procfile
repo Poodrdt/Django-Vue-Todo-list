6984fa3ec0184206bf6571e63ead59d9d80e8111
Poodrdt <yaroslav.info@gmail.com>
2018-06-07 11:05:12 +0300

    gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python src/manage.py runserver 0.0.0.0:5000
+web: gunicorn myproject.wsgi
--- src/todo/models.py
+++ src/todo/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 class Task(models.Model):
     title = models.CharField(max_length=64)
