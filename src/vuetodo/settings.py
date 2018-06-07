d5eb057be618e4ae6f4f83cbb3eab255cfb1dd56
Poodrdt <yaroslav.info@gmail.com>
2018-06-07 14:36:07 +0300

    heroku_settings

--- src/vuetodo/settings.py
+++ src/vuetodo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,4 +124,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
